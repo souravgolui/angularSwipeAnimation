0f51a71cf485fefa971dffe380e18415c3da0043
Sourav Golui <souravgolui1992@gmail.com>
2019-09-13 17:47:59 +0530

    unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -7,6 +10,11 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatCardModule
+      ]
     }).compileComponents();
   }));
 
@@ -16,16 +24,79 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hammerAnimation'`, () => {
+  it('should test animation if swipe left of card', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('hammerAnimation');
+
+    app.startAnimation('rollOutLeft');
+    fixture.detectChanges();
+
+    expect(app.animateState).toEqual('rollOutLeft');
+
+    app.resetAnimation();
+    expect(app.animateState).toEqual('');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should test animation if swipe right of card', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    app.startAnimation('rollOutRight');
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to hammerAnimation!');
+
+    expect(app.animateState).toEqual('rollOutRight');
+
+    app.resetAnimation();
+    expect(app.animateState).toEqual('');
+  });
+
+  it('should test animation if swipe up of card', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    app.startAnimation('zoomOutUp');
+    fixture.detectChanges();
+
+    expect(app.animateState).toEqual('zoomOutUp');
+
+    app.resetAnimation();
+    expect(app.animateState).toEqual('');
+  });
+
+  it('should test animation if swipe down of card', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    app.startAnimation('fadeOutDown');
+    fixture.detectChanges();
+
+    expect(app.animateState).toEqual('fadeOutDown');
+
+    app.resetAnimation();
+    expect(app.animateState).toEqual('');
+  });
+
+  it('should test animation if click on card', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    app.startAnimation('jello');
+    fixture.detectChanges();
+
+    expect(app.animateState).toEqual('jello');
+
+    app.resetAnimation();
+    expect(app.animateState).toEqual('');
+  });
+
+  it('should test if animateState is not empty', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    app.animateState = 'jello';
+    app.startAnimation('fadeOutDown');
+    fixture.detectChanges();
+
+    expect(app.animateState).toEqual('jello');
   });
 });
